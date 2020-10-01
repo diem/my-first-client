49f4da2b81b495c9001b25f931fc5a0e798a3198
maya <maya.marton@firstdag.com>
2020-10-01 18:39:43 +0300

    reorganize Main to demonstrate full work flow

--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -2,7 +2,10 @@ import example.*;
 import org.libra.AuthKey;
 import org.libra.PrivateKey;
 import org.libra.jsonrpctypes.JsonRpc.Account;
+import org.libra.jsonrpctypes.JsonRpc.Event;
 import org.libra.utils.CurrencyCode;
+
+import java.util.List;
 
 public class Main {
     /**
@@ -10,9 +13,15 @@ public class Main {
      * 1. Generate keys
      * 2. Create account (by mint for new account base on the keys we generated)
      * 3. Get account information
-     * 4. Add money to existing account (mint)
-     * 5. Generate second account for the benefit of the following transaction
-     * 6. Transfer money between accounts (peer to peer transaction)
+     * 4. Start events listener
+     * 5. Get new events (1)
+     * 6. Add money to existing account (mint)
+     * 7. Get new events (2)
+     * 8. Generate keys for second account
+     * 9. Create second account (for the benefit of the following transaction)
+     * 10. Transfer money between accounts (peer to peer transaction)
+     * 11. Get new events (3)
+     * 12. Close listener
      */
     public static void main(String args[]) {
         System.out.println("~1 Generate Keys");
@@ -27,31 +36,35 @@ public class Main {
         System.out.println("~3 Get account information");
         Account account = GetAccountInfoExample.getAccountInfo(accountAddress);
 
-        System.out.println("~ Get new events 1");
+        System.out.println("~4 Start event listener");
         GetEventsExample.start(account);
-        GetEventsExample.get(account);
 
-        System.out.println("~4 Add money to account");
+        System.out.println("~5 Get new events (1)");
+        List<Event> newEvents = GetEventsExample.get(account);
+        System.out.println(newEvents.size() + " new events was found");
+
+        System.out.println("~6 Add money to account");
         MintExample.mint(authKey, "270000000", CurrencyCode.LBR);
 
-        System.out.println("~ Get new events 2");
-        GetEventsExample.get(account);
+        System.out.println("~7 Get new events (2)");
+        newEvents = GetEventsExample.get(account);
+        System.out.println(newEvents.size() + " new events was found");
 
-        System.out.println("~5 Create receiver account");
+        System.out.println("~8 Generate Keys");
         PrivateKey receiverPrivateKey = GenerateKeysExample.generatePrivateKey();
         AuthKey receiverAuthKey = GenerateKeysExample.generateAuthKey(receiverPrivateKey);
 
+        System.out.println("~9 Create second account");
         MintExample.mint(receiverAuthKey, "2560000000", CurrencyCode.LBR);
 
-        System.out.println("~ Get new events 3");
-        GetEventsExample.get(account);
-
-        System.out.println("~6 Peer 2 peer transaction");
+        System.out.println("~10 Peer 2 peer transaction");
         SubmitPeerToPeerTransactionExample.submitPeerToPeerTransaction(privateKey, authKey, account, receiverAuthKey);
 
-        System.out.println("~ Get new events 4");
-        GetEventsExample.get(account);
+        System.out.println("~11 Get new events (3)");
+        newEvents = GetEventsExample.get(account);
+        System.out.println(newEvents.size() + " new events was found");
 
+        System.out.println("~12 Stop event listener");
         GetEventsExample.stop();
     }
 }
