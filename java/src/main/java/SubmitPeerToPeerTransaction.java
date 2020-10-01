8b91c066c9fb9d339cc2d80222bd78f4b3f7b0b0
maya <maya.marton@firstdag.com>
2020-10-01 10:57:22 +0300

    remove warnings and improve imports

--- java/src/main/java/SubmitPeerToPeerTransaction.java
+++ java/src/main/java/SubmitPeerToPeerTransaction.java
@@ -1,6 +1,7 @@
 import com.novi.serde.Bytes;
 import org.libra.*;
-import org.libra.jsonrpctypes.JsonRpc;
+import org.libra.jsonrpctypes.JsonRpc.Account;
+import org.libra.jsonrpctypes.JsonRpc.Transaction;
 import org.libra.stdlib.Helpers;
 import org.libra.types.RawTransaction;
 import org.libra.types.SignedTransaction;
@@ -10,7 +11,7 @@ import org.libra.utils.CurrencyCode;
 import static org.libra.Testnet.CHAIN_ID;
 
 public class SubmitPeerToPeerTransaction {
-    public static void submitPeerToPeerTransaction(LibraClient client, PrivateKey privateKey, AuthKey authKey, JsonRpc.Account account, AuthKey receiverAuthKey) {
+    public static void submitPeerToPeerTransaction(LibraClient client, PrivateKey privateKey, AuthKey authKey, Account account, AuthKey receiverAuthKey) {
         //Create script
         TransactionPayload script = generateScript(receiverAuthKey);
         //Create transaction
@@ -25,7 +26,7 @@ public class SubmitPeerToPeerTransaction {
 
     private static void waitForTransaction(LibraClient client, SignedTransaction st) {
         try {
-            JsonRpc.Transaction transaction = client.waitForTransaction(st, 100000);
+            Transaction transaction = client.waitForTransaction(st, 100000);
 
             System.out.println(transaction);
         } catch (LibraException e) {
@@ -41,24 +42,25 @@ public class SubmitPeerToPeerTransaction {
         }
     }
 
-    private static RawTransaction generateRawTransaction(AuthKey authKey, JsonRpc.Account account, TransactionPayload script) {
-        RawTransaction rawTransaction = new RawTransaction(
+    private static RawTransaction generateRawTransaction(AuthKey authKey, Account account, TransactionPayload script) {
+        return new RawTransaction(
                 authKey.accountAddress(),
-                account.getSequenceNumber(), script,
-                1000000L, 0L,
-                CurrencyCode.LBR, 100000000000L,
+                account.getSequenceNumber(),
+                script,
+                1000000L,
+                0L,
+                CurrencyCode.LBR,
+                100000000000L,
                 CHAIN_ID);
-        return rawTransaction;
     }
 
     private static TransactionPayload generateScript(AuthKey receiverAuthKey) {
-        TransactionPayload script = new TransactionPayload.Script(
+        return new TransactionPayload.Script(
                 Helpers.encode_peer_to_peer_with_metadata_script(
                         CurrencyCode.typeTag(CurrencyCode.LBR),
                         receiverAuthKey.accountAddress(),
                         1120000000L,
                         new Bytes(new byte[0]),
                         new Bytes(new byte[0])));
-        return script;
     }
 }
