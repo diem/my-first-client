c8147687b1bdfaa0878ba1a97dbb08a18cf65513
maya <maya.marton@firstdag.com>
2020-10-05 11:58:13 +0300

    add IntentIdentifier to Main - use it content for transaction

--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -1,10 +1,14 @@
 import example.*;
 import org.libra.AuthKey;
+import org.libra.IntentIdentifier;
 import org.libra.PrivateKey;
 import org.libra.jsonrpctypes.JsonRpc.Account;
 import org.libra.utils.CurrencyCode;
 
 import java.util.Queue;
+
+import static org.libra.AccountIdentifier.NetworkPrefix.TestnetPrefix;
+import static org.libra.IntentIdentifier.decode;
 
 public class Main {
     /**
@@ -18,37 +22,39 @@ public class Main {
      * 7. Get new events (2)
      * 8. Generate keys for second account
      * 9. Create second account (for the benefit of the following transaction)
-     * 10. Transfer money between accounts (peer to peer transaction)
-     * 11. Get new events (3)
-     * 12. Close listener
+     * 10. Generate IntentIdentifier
+     * 11. Deserialize IntentIdentifier
+     * 12. Transfer money between accounts (peer to peer transaction)
+     * 13. Get new events (3)
+     * 14. Close listener
      */
     public static void main(String[] args) {
         System.out.println("#1 Generate Keys");
-        PrivateKey privateKey = GenerateKeysExample.generatePrivateKey();
-        AuthKey authKey = GenerateKeysExample.generateAuthKey(privateKey);
+        PrivateKey senderPrivateKey = GenerateKeysExample.generatePrivateKey();
+        AuthKey senderAuthKey = GenerateKeysExample.generateAuthKey(senderPrivateKey);
 
         System.out.println("#2 Create account");
-        MintExample.mint(authKey, "1340000000", CurrencyCode.LBR);
+        MintExample.mint(senderAuthKey, "1340000000", CurrencyCode.LBR);
 
-        String accountAddress = GenerateKeysExample.extractAccountAddress(authKey);
+        String senderAccountAddress = GenerateKeysExample.extractAccountAddress(senderAuthKey);
 
         System.out.println("#3 Get account information");
-        Account account = GetAccountInfoExample.getAccountInfo(accountAddress);
+        Account senderAccount = GetAccountInfoExample.getAccountInfo(senderAccountAddress);
 
-        String receivedEventsKey = account.getReceivedEventsKey();
+        String eventsKey = senderAccount.getReceivedEventsKey();
         System.out.println("#4 Start event listener");
         GetEventsExample getEventsExample = new GetEventsExample();
-        getEventsExample.start(receivedEventsKey);
+        getEventsExample.start(eventsKey);
 
         System.out.println("#5 Get new events (1)");
-        Queue<GetEventsExample.EventExample> newEvents = getEventsExample.get(receivedEventsKey);
+        Queue<GetEventsExample.EventExample> newEvents = getEventsExample.get(eventsKey);
         System.out.println(newEvents.size() + " new events was found");
 
         System.out.println("#6 Add money to account");
-        MintExample.mint(authKey, "270000000", CurrencyCode.LBR);
+        MintExample.mint(senderAuthKey, "270000000", CurrencyCode.LBR);
 
         System.out.println("#7 Get new events (2)");
-        newEvents = getEventsExample.get(receivedEventsKey);
+        newEvents = getEventsExample.get(eventsKey);
         System.out.println(newEvents.size() + " new events was found");
 
         System.out.println("#8 Generate Keys");
@@ -58,14 +64,25 @@ public class Main {
         System.out.println("#9 Create second account");
         MintExample.mint(receiverAuthKey, "2560000000", CurrencyCode.LBR);
 
-        System.out.println("#10 Peer 2 peer transaction");
-        SubmitPeerToPeerTransactionExample.submitPeerToPeerTransaction(privateKey, authKey, account, receiverAuthKey);
+        System.out.println("#10 Generate IntentIdentifier");
+        String intentIdentifierString = GenerateIntentIdentifierExample.generateIntentIdentifier(receiverAuthKey.accountAddress(), 130000000L, CurrencyCode.LBR);
 
-        System.out.println("#11 Get new events (3)");
-        newEvents = getEventsExample.get(receivedEventsKey);
+        System.out.println("#11 Deserialize IntentIdentifier");
+        IntentIdentifier intentIdentifier = decode(TestnetPrefix, intentIdentifierString);
+
+        System.out.println("#12 Peer 2 peer transaction");
+        SubmitPeerToPeerTransactionExample.submitPeerToPeerTransaction(senderPrivateKey,
+                intentIdentifier.getAmount(),
+                intentIdentifier.getAccountIdentifier().getAccountAddress(),
+                senderAuthKey.accountAddress(),
+                senderAccount.getSequenceNumber(),
+                intentIdentifier.getCurrency());
+
+        System.out.println("#13 Get new events (3)");
+        newEvents = getEventsExample.get(eventsKey);
         System.out.println(newEvents.size() + " new events was found");
 
-        System.out.println("#12 Stop event listener");
+        System.out.println("#14 Stop event listener");
         getEventsExample.stop();
     }
 }
--- java/src/main/java/example/GenerateIntentIdentifierExample.java
+++ java/src/main/java/example/GenerateIntentIdentifierExample.java
@@ -7,10 +7,13 @@ import org.libra.types.AccountAddress;
 import static org.libra.AccountIdentifier.NetworkPrefix.TestnetPrefix;
 
 public class GenerateIntentIdentifierExample {
-    public static void generateIntentIdentifier(AccountAddress address, long amount, String currency) {
+    public static String generateIntentIdentifier(AccountAddress address, long amount, String currency) {
         AccountIdentifier accountIdentifier = new AccountIdentifier(TestnetPrefix, address);
         IntentIdentifier intentIdentifier = new IntentIdentifier(accountIdentifier, currency, amount);
         String encodedIntentIdentifier = intentIdentifier.encode();
+
         System.out.println("encodedIntentIdentifier: " + encodedIntentIdentifier);
+
+        return encodedIntentIdentifier;
     }
 }
