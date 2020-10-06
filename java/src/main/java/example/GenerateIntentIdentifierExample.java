80c5f93d4ddc017cdc311ed843b013d4e1e67e28
maya <maya.marton@firstdag.com>
2020-10-06 17:04:20 +0300

    add main in GenerateIntentIdentifierExample

--- java/src/main/java/example/GenerateIntentIdentifierExample.java
+++ java/src/main/java/example/GenerateIntentIdentifierExample.java
@@ -1,12 +1,23 @@
 package example;
 
 import org.libra.AccountIdentifier;
+import org.libra.AuthKey;
 import org.libra.IntentIdentifier;
+import org.libra.PrivateKey;
 import org.libra.types.AccountAddress;
+import org.libra.utils.CurrencyCode;
 
 import static org.libra.AccountIdentifier.NetworkPrefix.TestnetPrefix;
 
 public class GenerateIntentIdentifierExample {
+    public static void main(String[] args) {
+        PrivateKey privateKey = GenerateKeysExample.generatePrivateKey();
+        AuthKey authKey = GenerateKeysExample.generateAuthKey(privateKey);
+        String intentIdentifier = generateIntentIdentifier(authKey.accountAddress(), 130000000L, CurrencyCode.LBR);
+
+        System.out.println(intentIdentifier);
+    }
+
     public static String generateIntentIdentifier(AccountAddress address, long amount, String currency) {
         AccountIdentifier accountIdentifier = new AccountIdentifier(TestnetPrefix, address);
         IntentIdentifier intentIdentifier = new IntentIdentifier(accountIdentifier, currency, amount);
