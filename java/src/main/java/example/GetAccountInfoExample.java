b642a07ee2185339d8f78c50ddcd6c0f477958bd
maya <maya.marton@firstdag.com>
2020-10-06 17:13:03 +0300

    add main in GetAccountInfoExample

--- java/src/main/java/example/GetAccountInfoExample.java
+++ java/src/main/java/example/GetAccountInfoExample.java
@@ -1,11 +1,25 @@
 package example;
 
+import org.libra.AuthKey;
 import org.libra.LibraClient;
 import org.libra.LibraException;
+import org.libra.PrivateKey;
 import org.libra.jsonrpc.LibraJsonRpcClient;
 import org.libra.jsonrpctypes.JsonRpc.Account;
+import org.libra.utils.CurrencyCode;
 
 public class GetAccountInfoExample {
+    public static void main(String[] args) {
+        PrivateKey privateKey = GenerateKeysExample.generatePrivateKey();
+        AuthKey authKey = GenerateKeysExample.generateAuthKey(privateKey);
+        MintExample.mint(authKey, "1340000000", CurrencyCode.LBR);
+        String accountAddress = GenerateKeysExample.extractAccountAddress(authKey);
+
+        Account account = getAccountInfo(accountAddress);
+
+        System.out.println(account);
+    }
+
     public static Account getAccountInfo(String accountAddress) {
         //Connect to testnet
         LibraClient client = new LibraJsonRpcClient(Testnet.NET_URL, Testnet.CHAIN_ID);
