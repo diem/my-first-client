74ce3b67a76d1a4e490dffb504bfcd52f2b3ae28
Maya Marton <maya.marton@firstdag.com>
2020-10-07 15:47:32 +0300

    WIP: 4. python - intent (#11)
    * intent
    
    * make intent example include creation and deserialization of the intent identifier + add comments and main
    
    * WIP: 5. submit transaction (#9)
    
    * submit peer to peer transaction
    
    * add main and comment in submit_peer_to_peer_transaction_example
    
    * WIP: 6. python - events (#10)
    
    * start get events
    
    * working on pooling...
    
    * cover job with Timeloop object as it suppose to work
    
    * stop events listener in main
    
    * events subscription
    
    * clear Pipfile
    
    * revert Pipfile.lock
    
    * fix merge

---
+++ python/src/get_events_example.py
@@ -0,0 +1,60 @@
+import time
+from random import randrange
+from threading import Thread
+
+from libra import jsonrpc
+
+from src.generate_keys_example import extract_account_address, generate_auth_key
+from src.get_account_info_example import get_account_info
+from src.mint_example import mint
+from src.testnet import JSON_RPC_URL
+
+client = jsonrpc.Client(JSON_RPC_URL)
+
+"""get_events_example demonstrates how to subscribe to a specific events stream base on events key"""
+
+
+def main():
+    # create new account
+    auth_key = generate_auth_key()
+    mint(auth_key.hex(), 110000000, "LBR")
+    account_address = extract_account_address(auth_key)
+
+    # get account events key
+    account = get_account_info(account_address)
+    events_key = account.received_events_key
+
+    # start minter to demonstrates events creation
+    start_minter(auth_key)
+
+    # demonstrates events subscription
+    subscribe(events_key)
+
+
+def subscribe_(events_key):
+    start = 0
+
+    for x in range(0, 15):
+        events = client.get_events(events_key, start, 10)
+        start += len(events)
+        print(events)
+        time.sleep(3)
+
+
+def minter(auth_key):
+    for x in range(0, 10):
+        amount = randrange(10, 19) * 10000000
+        mint(auth_key.hex(), amount, "LBR")
+        time.sleep(1)
+
+
+def subscribe(events_key):
+    Thread(target=subscribe_, args=(events_key,)).start()
+
+
+def start_minter(auth_key):
+    Thread(target=minter, args=(auth_key,)).start()
+
+
+if __name__ == "__main__":
+    main()
