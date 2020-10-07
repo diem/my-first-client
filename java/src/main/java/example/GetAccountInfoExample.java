7044d604c336b7ecbe11182283acf892e9f59d73
maya <maya.marton@firstdag.com>
2020-10-07 11:30:14 +0300

    add javadocs and comments in GetAccountInfoExample

--- java/src/main/java/example/GetAccountInfoExample.java
+++ java/src/main/java/example/GetAccountInfoExample.java
@@ -8,13 +8,18 @@ import org.libra.jsonrpc.LibraJsonRpcClient;
 import org.libra.jsonrpctypes.JsonRpc.Account;
 import org.libra.utils.CurrencyCode;
 
+/**
+ * GetAccountInfoExample demonstrates the required operation to retrieve account information from the Libra blockchain
+ */
 public class GetAccountInfoExample {
     public static void main(String[] args) {
+        //create account
         PrivateKey privateKey = GenerateKeysExample.generatePrivateKey();
         AuthKey authKey = GenerateKeysExample.generateAuthKey(privateKey);
         MintExample.mint(authKey, "1340000000", CurrencyCode.LBR);
         String accountAddress = GenerateKeysExample.extractAccountAddress(authKey);
 
+        //get account information
         Account account = getAccountInfo(accountAddress);
 
         System.out.println(account);
