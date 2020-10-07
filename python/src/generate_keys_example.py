fbd9ade0fddce20efe739bfeb17ce9d8b0477ffd
Maya Marton <maya.marton@firstdag.com>
2020-10-07 15:48:40 +0300

    WIP: 1. generate keys (#5)
    * generate_keys_example
    
    * generate receiver keys
    
    * add main in generate_keys_example and clear main example
    
    * improve generate_auth_key
    
    * organize PR's
    
    * WIP: 2. mint (#6)
    
    * mint
    
    * mint
    
    * add second mint call
    
    * fix mint (#6) and create receiver account
    
    * add main in mint_example
    
    * fix comment
    
    * fix merge
    
    * organize PR's
    
    * WIP: 3. get account (#7)
    
    * get account info
    
    * add main and comments to get_account_info_example
    
    * fix merge
    
    * WIP: 4. python - intent (#11)
    
    * intent
    
    * make intent example include creation and deserialization of the intent identifier + add comments and main
    
    * WIP: 5. submit transaction (#9)
    
    * submit peer to peer transaction
    
    * add main and comment in submit_peer_to_peer_transaction_example
    
    * WIP: 6. python - events (#10)
    
    * start get events
    
    * working on pooling...
    
    * cover job with Timeloop object as it suppose to work
    
    * stop events listener in main
    
    * events subscription
    
    * clear Pipfile
    
    * revert Pipfile.lock
    
    * fix merge

---
+++ python/src/generate_keys_example.py
@@ -0,0 +1,37 @@
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
+from libra import AuthKey, utils
+
+"""generate_keys_example demonstrates the required steps to generate keys for an account on the Libra blockchain"""
+
+
+def main():
+    private_key = generate_private_key()
+
+    print(utils.public_key_bytes(private_key.public_key()).hex())
+
+    auth_key = generate_auth_key(private_key)
+
+    print(auth_key.hex())
+
+
+def generate_private_key() -> Ed25519PrivateKey:
+    return Ed25519PrivateKey.generate()
+
+
+def generate_auth_key(private_key: Ed25519PrivateKey = None) -> AuthKey:
+    if not private_key:
+        private_key = generate_private_key()
+
+    auth_key = AuthKey.from_public_key(private_key.public_key())
+    account_address = extract_account_address(auth_key)
+    print("~ Generated address: " + account_address)
+
+    return auth_key
+
+
+def extract_account_address(auth_key: AuthKey) -> str:
+    return utils.account_address_hex(auth_key.account_address())
+
+
+if __name__ == "__main__":
+    main()
--- python/src/my_first_client.py
+++ python/src/my_first_client.py
@@ -1,5 +1,66 @@
+"""
+ This code demonstrates basic flow for working with the LibraClient.
+ 1. Generate keys
+ 2. Create account - by minting
+ 3. Get account information
+ 4. Start events listener
+ 5. Add money to existing account (mint)
+ 6. Generate keys for second account
+ 7. Create second account (for the benefit of the following transaction)
+ 8. Generate IntentIdentifier
+ 9. Deserialize IntentIdentifier
+ 10. Transfer money between accounts (peer to peer transaction)
+"""
+from libra import AuthKey
+
+from src.generate_keys_example import generate_private_key, generate_auth_key, extract_account_address
+from src.get_account_info_example import get_account_info
+from src.get_events_example import subscribe
+from src.intent_identifier_example import generate_intent_identifier, decode_intent
+from src.mint_example import mint
+from src.submit_peer_to_peer_transaction_example import submit_peer_to_peer_transaction
+
+
 def main():
-    print("Hello World!")
+    print("#1 Generate Keys")
+    sender_private_key = generate_private_key()
+    sender_auth_key: AuthKey = generate_auth_key(sender_private_key)
+
+    print("#2 Create account")
+    mint(sender_auth_key.hex(), 1340000000, "LBR")
+
+    sender_account_address = extract_account_address(sender_auth_key)
+
+    print("#3 Get account information")
+    sender_account = get_account_info(sender_account_address)
+
+    events_key = sender_account.received_events_key
+    print("#4 Start event listener")
+    subscribe(events_key)
+
+    print("#5 Add money to account")
+    mint(sender_auth_key.hex(), 270000000, "LBR")
+
+    print("#6 Generate Keys")
+    receiver_private_key = generate_private_key()
+    receiver_auth_key: AuthKey = generate_auth_key(receiver_private_key)
+
+    print("#7 Create second account")
+    mint(receiver_auth_key.hex(), 2560000000, "LBR")
+
+    print("#8 Generate IntentIdentifier")
+    encoded_intent_identifier = generate_intent_identifier(receiver_auth_key.account_address(), 130000000, "LBR")
+
+    print("#9 Deserialize IntentIdentifier")
+    intent_identifier = decode_intent(encoded_intent_identifier)
+
+    print("#10 Peer 2 peer transaction")
+    submit_peer_to_peer_transaction(sender_private_key,
+                                    intent_identifier.amount,
+                                    receiver_auth_key.account_address(),
+                                    sender_auth_key.account_address(),
+                                    sender_account.sequence_number,
+                                    "LBR")
 
 
 if __name__ == "__main__":
