ae1aa28bc005505bdd51e982fa5c6f1bf15faa7c
maya <maya.marton@firstdag.com>
2020-10-07 13:00:53 +0300

    PR comments - GetEventsExample

--- java/src/main/java/example/GetEventsExample.java
+++ java/src/main/java/example/GetEventsExample.java
@@ -9,6 +9,7 @@ import org.libra.jsonrpctypes.JsonRpc.Event;
 import org.libra.utils.CurrencyCode;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * GetEventsExample demonstrates how to subscribe to a specific events stream base on events key
@@ -33,23 +34,21 @@ public class GetEventsExample {
         subscribe(eventsKey);
     }
 
-    private static List<Event> getEventsByKey(String eventsKey) {
-        try {
-            return client.getEvents(eventsKey, start, 10);
-        } catch (LibraException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public static void subscribe(String eventsKey) {
         Runnable listener = () -> {
             for (int i = 0; i < 15; i++) {
-                List<Event> events = getEventsByKey(eventsKey);
+                List<Event> events;
+
+                try {
+                    events = client.getEvents(eventsKey, start, 10);
+                } catch (LibraException e) {
+                    throw new RuntimeException(e);
+                }
 
                 start += events.size();
                 System.out.println(events);
                 try {
-                    Thread.sleep(1);
+                    Thread.sleep(3_000);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
@@ -63,7 +62,13 @@ public class GetEventsExample {
     private static void startMinter(AuthKey authKey) {
         Runnable minter = () -> {
             for (int i = 0; i < 10; i++) {
-                MintExample.mint(authKey, "110000000", CurrencyCode.LBR);
+                int amount = ThreadLocalRandom.current().nextInt(10, 19) * 10000000;
+                MintExample.mint(authKey, String.valueOf(amount), CurrencyCode.LBR);
+                try {
+                    Thread.sleep(1_000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
         };
 
